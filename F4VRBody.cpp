6515d384c91b85163b998001857044c56ae5ac7a
rollingrock <rollingrock16@gmail.com>
2020-10-26 21:33:40 -0500

    Added skeleton class and tree dump function to traverse the tree

--- F4VRBody.cpp
+++ F4VRBody.cpp
@@ -2,13 +2,63 @@
 
 
 namespace F4VRBody {
+
 	bool isLoaded = false;
-	BSFadeNode* skelly;
+
+	class Skeleton {
+	public:
+		Skeleton() : _root(nullptr)
+		{}
+
+		Skeleton(BSFadeNode *a_node) : _root(a_node)
+		{}
+
+		BSFadeNode* getRoot() {
+			return _root;
+		}
+
+		void printChildren(NiNode *child, std::string padding) {
+			padding += "....";
+			_MESSAGE("%s%s : children = %d : local z = %f : world z = %f", padding.c_str(), child->m_name.c_str(), child->m_children.m_emptyRunStart, child->m_localTransform.pos.z, child->m_worldTransform.pos.z);
+
+			for (auto i = 0; i < child->m_children.m_emptyRunStart; ++i) {
+				auto nextNode = child->m_children.m_data[i] ? child->m_children.m_data[i]->GetAsNiNode() : nullptr;
+				if (nextNode) {
+					this->printChildren(nextNode, padding);
+				}
+			}
+		}
+
+		void printNodes() {
+			// print root node info first
+			_MESSAGE("%s : children = %d : local z = %f : world z = %f", _root->m_name.c_str(), _root->m_children.m_emptyRunStart, _root->m_localTransform.pos.z, _root->m_worldTransform.pos.z);
+
+			std::string padding = "";
+
+			for (auto i = 0; i < _root->m_children.m_emptyRunStart; ++i) {
+				auto nextNode = _root->m_children.m_data[i] ? _root->m_children.m_data[i]->GetAsNiNode() : nullptr;
+				if (nextNode) {
+					this->printChildren(nextNode, padding);
+				}
+			}
+		}
+
+
+
+
+	private:
+		BSFadeNode *_root;
+		NiPoint3   _lastPos;
+
+		std::vector<NiNode> _nodes;
+	};
+
+	Skeleton *playerSkelly = nullptr;
 
 	bool setSkelly() {
 		if ((*g_player)->unkF0 && (*g_player)->unkF0->rootNode) {
-			skelly = (BSFadeNode*)(*g_player)->unkF0->rootNode;	
-			_MESSAGE("skeleton = %016I64X", skelly);
+			playerSkelly = new Skeleton((BSFadeNode*)(*g_player)->unkF0->rootNode);
+			_MESSAGE("skeleton = %016I64X", playerSkelly->getRoot());
 			return true;
 		}
 		else {
@@ -21,10 +71,12 @@ namespace F4VRBody {
 			return;
 		}
 
-		if (!skelly) {
+		if (!playerSkelly) {
 			if (!setSkelly()) {
 				return;
 			}
+			playerSkelly->printNodes();
+			
 		}
 
 		// do stuff now
@@ -33,15 +85,7 @@ namespace F4VRBody {
 
 	void startUp() {
 		_MESSAGE("Starting up F4Body");
-		PlayerCharacter* pc = *g_player;
 		isLoaded = true;
-
-		if (pc) {
-			if (pc->unkF0 && pc->unkF0->rootNode) {
-				isLoaded = true;
-				skelly = (BSFadeNode*)pc->unkF0->rootNode;
-			}
-		}
 		return;
 	}
 }
