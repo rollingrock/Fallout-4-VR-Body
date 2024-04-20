0cd840e7dcf7bdc19de557d30e01c39b443e8ef9
rollingrock <rollingrock16@gmail.com>
2024-04-20 09:14:37 -0500

    adding vr class for openvr api

---
+++ VR.cpp
@@ -0,0 +1,78 @@
+#include "VR.h"
+#include "matrix.h"
+
+
+namespace VRHook {
+
+	RelocPtr<uint64_t*> vrDataStruct(0x59429c0);
+
+	void HmdMatrixToNiTransform(NiTransform* a_transform, vr::TrackedDevicePose_t* a_pose) {
+		using func_t = decltype(&HmdMatrixToNiTransform);
+		RelocAddr<func_t> func(0x1bab210);
+		return func(a_transform, a_pose);
+	}
+
+	void VRSystem::getTrackerNiTransformByName(std::string trackerName, NiTransform* transform) {
+		vr::TrackedDevicePose_t pose = renderPoses[viveTrackers[trackerName]];
+		HmdMatrixToNiTransform(transform, &pose);
+
+		if (vrDataStruct != nullptr && roomNode != nullptr) {
+			NiMatrix43* worldSpaceMat = (NiMatrix43*)((char*)(*vrDataStruct) + 0x210);
+			NiPoint3* worldSpaceVec = (NiPoint3*)((char*)(*vrDataStruct) + 0x158);
+			transform->pos = transform->pos - *worldSpaceVec;
+			transform->pos = *worldSpaceMat * transform->pos;
+	//		transform->pos = roomNode->m_worldTransform.rot * transform->pos * roomNode->m_worldTransform.scale;
+		}
+
+	}
+	
+	void VRSystem::getTrackerNiTransformByIndex(vr::TrackedDeviceIndex_t idx, NiTransform* transform) {
+		vr::TrackedDevicePose_t pose = renderPoses[idx];
+		HmdMatrixToNiTransform(transform, &pose);
+
+		if (vrDataStruct != nullptr && roomNode != nullptr) {
+			NiMatrix43* worldSpaceMat = (NiMatrix43*)((char*)(*vrDataStruct) + 0x210);
+			NiPoint3* worldSpaceVec = (NiPoint3*)((char*)(*vrDataStruct) + 0x158);
+			transform->pos = transform->pos - *worldSpaceVec;
+			transform->pos = *worldSpaceMat * transform->pos;
+	//		transform->pos = roomNode->m_worldTransform.rot * transform->pos * roomNode->m_worldTransform.scale;
+		}
+	}
+
+	void VRSystem::getControllerNiTransformByName(std::string trackerName, NiTransform* transform) {
+		vr::TrackedDevicePose_t pose = renderPoses[controllers[trackerName]];
+		HmdMatrixToNiTransform(transform, &pose);
+
+		if (vrDataStruct != nullptr && roomNode != nullptr) {
+			NiMatrix43* worldSpaceMat = (NiMatrix43*)((char*)(*vrDataStruct) + 0x210);
+			NiPoint3* worldSpaceVec = (NiPoint3*)((char*)(*vrDataStruct) + 0x158);
+			transform->pos = transform->pos - *worldSpaceVec;
+			transform->pos = *worldSpaceMat * transform->pos;
+		}
+	}
+	
+	void VRSystem::debugPrint() {
+		vr::VRCompositorError error = vrHook->GetVRCompositor()->GetLastPoses((vr::TrackedDevicePose_t*)renderPoses, vr::k_unMaxTrackedDeviceCount, (vr::TrackedDevicePose_t*)gamePoses, vr::k_unMaxTrackedDeviceCount);
+		if (error && error != vr::EVRCompositorError::VRCompositorError_None)
+			_MESSAGE("Error while retriving game poses!");
+
+		for (auto i : viveTrackers) {
+			vr::TrackedDevicePose_t renderMat = renderPoses[i.second];
+			vr::TrackedDevicePose_t gameMat = gamePoses[i.second];
+
+			NiTransform renderTran;
+			NiTransform gameTran;
+			HmdMatrixToNiTransform(&renderTran, &renderMat);
+
+			_MESSAGE("%d : %s --> render = %f %f %f |||| game = %f %f %f", i.second, i.first.c_str(), renderTran.pos.x,
+				renderTran.pos.y,
+				renderTran.pos.z,
+				gameMat.mDeviceToAbsoluteTracking.m[0][3],
+				gameMat.mDeviceToAbsoluteTracking.m[1][3],
+				gameMat.mDeviceToAbsoluteTracking.m[2][3]
+				);
+		}
+	}
+
+
+}
---
+++ VR.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "api/PapyrusVRAPI.h"
+#include "api/VRManagerAPI.h"
+
+#include "f4se/NiTypes.h"
+#include "f4se/NiNodes.h"
+
+#include <memory>
+
+
+namespace VRHook {
+
+
+	class VRSystem {
+	public:
+		VRSystem() {
+			leftPacket = 0;
+			rightPacket = 0;
+
+			vrHook = RequestOpenVRHookManagerObject();
+
+			for (vr::TrackedDeviceIndex_t i = 0; i < vr::k_unMaxTrackedDeviceCount; i++) {
+				auto dc = vrHook->GetVRSystem()->GetTrackedDeviceClass(i);
+				if (dc == vr::ETrackedDeviceClass::TrackedDeviceClass_GenericTracker) {
+					viveTrackers.insert({ getProperty(vr::ETrackedDeviceProperty::Prop_ModelNumber_String, i), i } );
+				}
+				else if (dc == vr::ETrackedDeviceClass::TrackedDeviceClass_Controller) {
+					std::string prop = getProperty(vr::ETrackedDeviceProperty::Prop_ModelNumber_String, i);
+					if (prop.find("Right") != std::string::npos) {
+						controllers.insert({ "Right", i});
+					}
+					else if (prop.find("Left") != std::string::npos) {
+						controllers.insert({ "Left", i});
+					}
+				}
+				else  if (dc == vr::ETrackedDeviceClass::TrackedDeviceClass_HMD) {
+					controllers.insert({ "HMD", i });
+				}
+			}
+			roomNode = nullptr;
+		}
+
+		inline void setRoomNode(NiNode* a_node) {
+			roomNode = a_node;
+		}
+
+		inline OpenVRHookManagerAPI* getHook() {
+			return vrHook;
+		}
+
+		inline void updatePoses() {
+			vr::VRCompositorError error = vrHook->GetVRCompositor()->GetLastPoses((vr::TrackedDevicePose_t*)renderPoses, vr::k_unMaxTrackedDeviceCount, (vr::TrackedDevicePose_t*)gamePoses, vr::k_unMaxTrackedDeviceCount);
+		}
+
+		inline bool viveTrackersPresent() const { return !viveTrackers.empty(); }
+	
+		inline void setVRControllerState() {
+			if (vrHook != nullptr) {
+
+				vr::TrackedDeviceIndex_t lefthand = vrHook->GetVRSystem()->GetTrackedDeviceIndexForControllerRole(vr::ETrackedControllerRole::TrackedControllerRole_LeftHand);
+				vr::TrackedDeviceIndex_t righthand = vrHook->GetVRSystem()->GetTrackedDeviceIndexForControllerRole(vr::ETrackedControllerRole::TrackedControllerRole_RightHand);
+
+				vrHook->GetVRSystem()->GetControllerState(lefthand, &leftControllerState, sizeof(vr::VRControllerState_t));
+				vrHook->GetVRSystem()->GetControllerState(righthand, &rightControllerState, sizeof(vr::VRControllerState_t));
+
+				if (rightControllerState.unPacketNum != rightPacket) {
+				}
+				if (leftControllerState.unPacketNum != leftPacket) {
+				}
+			}
+		}
+
+		inline vr::VRControllerState_t getControllerState(bool retLeft) {
+			return retLeft ? leftControllerState : rightControllerState;
+		}
+
+		void getTrackerNiTransformByName(std::string trackerName, NiTransform* transform);
+		void getTrackerNiTransformByIndex(vr::TrackedDeviceIndex_t idx, NiTransform* transform);
+		void getControllerNiTransformByName(std::string trackerName, NiTransform* transform);
+		void debugPrint();
+
+	private:
+
+		inline std::string getProperty(vr::ETrackedDeviceProperty property, vr::TrackedDeviceIndex_t idx) {
+				const uint32_t bufSize = vr::k_unMaxPropertyStringSize;
+				std::unique_ptr<char*> pchValue = std::make_unique<char*>(new char[bufSize]);
+				vr::TrackedPropertyError pError = vr::TrackedPropertyError::TrackedProp_NotYetAvailable;
+
+				vrHook->GetVRSystem()->GetStringTrackedDeviceProperty(idx, property, *pchValue.get(), bufSize, &pError);
+
+				return std::string(*pchValue.get());
+		}
+
+		uint32_t leftPacket;
+		uint32_t rightPacket;
+		OpenVRHookManagerAPI* vrHook;
+		vr::VRControllerState_t rightControllerState;
+		vr::VRControllerState_t leftControllerState;
+		vr::TrackedDevicePose_t renderPoses[vr::k_unMaxTrackedDeviceCount]; //Used to store available poses
+		vr::TrackedDevicePose_t gamePoses[vr::k_unMaxTrackedDeviceCount]; //Used to store available poses
+		std::map<std::string, vr::TrackedDeviceIndex_t> viveTrackers;
+		std::map<std::string, vr::TrackedDeviceIndex_t> controllers;
+		NiNode* roomNode;
+	};
+}
